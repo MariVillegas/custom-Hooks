22eeea9cd17f2837112c604e35b84bf79b025dfb
Mariana <mva.mari29@gmail.com>
2023-04-19 15:47:21 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,63 @@
+import { todoReducer } from "../08-useReducer/todoReducer";
+import { useEffect, useReducer } from "react"
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+};
+
+export const useTodo = ( initialState = [] ) => {
+    
+    const [ todos, dispatchTodo ] = useReducer( todoReducer, initialState, init);
+  
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));      
+      }, [todos])
+    
+
+    const handleNewTodo = ( todo ) => {
+
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatchTodo ( action );
+    };
+
+    const handleDeleteTodo = ( id ) => {
+        
+        const action = {
+            type: '[TODO] Remove Todo',
+            payload: id
+        }
+
+        dispatchTodo ( action );
+    };
+
+    const handleToggleTodo = ( id ) => {
+        
+        const action = {
+            type: '[TODO] Toggle Todo',
+            payload: id
+        }
+
+        dispatchTodo ( action );
+    };
+
+    // const todosCount = ( todos ) => {
+    //     return todos.length;
+    // }
+
+    // const pendingTodosCount = ( todos ) => {
+    //     return todos.filter(todo => !todo.done).length;
+    // }
+  
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+}
